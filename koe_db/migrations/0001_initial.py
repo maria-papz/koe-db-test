635ca7222327d87026c2e6dc6410448c5547dbcb
Maria <maria.papageorgiou.2001@outlook.com>
2025-09-09 12:20:32 +0300

    migration play

--- koe_db/migrations/0001_initial.py
+++ koe_db/migrations/0001_initial.py
@@ -10,7 +10,8 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
+        ('auth', '0001_initial'),
+        ('contenttypes', '0001_initial'),
     ]
 
     operations = [
